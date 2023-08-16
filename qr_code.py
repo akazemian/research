a5ff17c1a5f0969d27a9ebdf55b31d2c11209dde
akazemian <atlaskazemian@gmail.com>
2023-08-16 12:58:54 -0400

    updating poster

--- qr_code.py
+++ qr_code.py
@@ -2,9 +2,9 @@
 
 import qrcode
 
-file_name = 'ccn_2023_poster'
-path = f"https://akazemian.github.io/research/{file_name}.pdf"
-
+file_name = 'project_git_repo'
+#path = f"https://akazemian.github.io/research/{file_name}.pdf"
+path = 'https://github.com/akazemian/random_models_of_visual_cortex'
 # Generate QR code
 qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
 qr.add_data(path)
@@ -13,4 +13,6 @@ qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white")
 
 # Save the image or display it
-img.save(f"{file_name}.png")
+img.save(f"/home/atlask/Desktop/research/{file_name}.png")
+
+print('QR code is generated')
